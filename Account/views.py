5efab0cdb7f8babf5be8ea24cb00b0c6956f3207
development0261-at-985142018095 <development0261@gmail.com>
2021-09-11 10:57:47 +0530

    city changes

--- Account/views.py
+++ Account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import requests
 
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
@@ -13,19 +14,16 @@ def register(request):
 def course_details(request):
     URL = requests.get("https://ipinfo.io/")
     data = URL.json()
-    city = ["New York City","Seattle", "Philadelphia", "Boston", "Chicago", "Los Angeles", "San Diego", "Dallas", "Houston", "Atlanta", "Miami"]
-
-    country = ["US","IN"]
+    city = ["New York City", "Philadelphia", "Toronto", "Boston", "Chicago", "Los Angeles", "San Diego", "Dallas",
+            "Houston", "Atlanta", "Miami"]
     context = []
-
-    if data['country'] in country:
+    if data['city'] in city:
         context.append({
-            'country' : data['country']
+            'city': data['city']
         })
-
     else:
         context.append({
-            'country': "Unknown"
+            'city': "Unknown"
         })
     print(context)
-    return render(request, 'course-details.html', {'city_list':context[0]['country']})
+    return render(request, 'course-details.html', {'city_list': context[0]['city']})
